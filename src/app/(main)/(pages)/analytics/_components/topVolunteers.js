95da5b329dd545d70ddbab35e2988927fb031a2a
yfxstudios <yfxstudios@gmail.com>
2024-06-22 21:36:36 -0700

    Sort volunteers

--- src/app/(main)/(pages)/analytics/_components/topVolunteers.js
+++ src/app/(main)/(pages)/analytics/_components/topVolunteers.js
@@ -1,7 +1,7 @@
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import { Card, CardContent, CardHeader } from '@/components/ui/card'
-import { Separator } from '@/components/ui/separator'
-import React from 'react'
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import React from "react";
 
 const TopVolunteers = ({ users }) => {
   return (
@@ -13,33 +13,47 @@ const TopVolunteers = ({ users }) => {
         </div>
       </CardHeader>
       <CardContent>
-        {users.map((user, index) => {
-          return (
-            <>
-              <div key={index} className="flex items-center justify-between p-4 rounded-lg">
-                <div className="flex items-center">
-                  <Avatar>
-                    <AvatarImage src={user.image} alt="user" />
-                    <AvatarFallback>
-                      <p>{user.first_name[0]}{user.last_name[0]}</p>
-                    </AvatarFallback>
-                  </Avatar>
-                  <div className="ml-4">
-                    <h2 className="text-lg font-semibold">{user.first_name} {user.last_name}</h2>
-                    <p className="text-sm text-gray-500">{user.email}</p>
+        {users
+          .sort((a, b) => b.time - a.time)
+          .slice(0, 5)
+          .map((user, index) => {
+            return (
+              <>
+                <div
+                  key={index}
+                  className="flex items-center justify-between p-4 rounded-lg"
+                >
+                  <div className="flex items-center">
+                    <Avatar>
+                      <AvatarImage src={user.image} alt="user" />
+                      <AvatarFallback>
+                        <p>
+                          {user.first_name[0]}
+                          {user.last_name[0]}
+                        </p>
+                      </AvatarFallback>
+                    </Avatar>
+                    <div className="ml-4">
+                      <h2 className="text-lg font-semibold">
+                        {user.first_name} {user.last_name}
+                      </h2>
+                      <p className="text-sm text-gray-500">{user.email}</p>
+                    </div>
                   </div>
+                  <p className="text-lg font-semibold">
+                    {Math.floor(user.time / 60)}:
+                    {user.time % 60 < 10
+                      ? `0${user.time % 60}`
+                      : user.time % 60}
+                  </p>
                 </div>
-                <p className="text-lg font-semibold">
-                  {Math.floor(user.time / 60)}:{user.time % 60 < 10 ? `0${user.time % 60}` : user.time % 60}
-                </p>
-              </div>
-              {index !== users.length - 1 && <Separator />}
-            </>
-          )
-        })}
+                {index !== users.length - 1 && <Separator />}
+              </>
+            );
+          })}
       </CardContent>
     </Card>
-  )
-}
+  );
+};
 
-export default TopVolunteers
+export default TopVolunteers;
