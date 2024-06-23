3c047c41e0c19b1567cc2c7244daeb859e370207
yfxstudios <yfxstudios@gmail.com>
2024-06-22 21:48:57 -0700

    Edge test

---
+++ src/app/api/remind/route.js
@@ -0,0 +1,7 @@
+export const dynamic = 'force-dynamic'; // static by default, unless reading the request
+
+export function GET(request) {
+  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+}
+
+export const runtime = 'edge';
